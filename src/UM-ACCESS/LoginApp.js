341068e2378b76e2ebaf2609c013a1bb84d73f42
advyth <advythashok@gmail.com>
2019-03-20 15:31:09 +0530

    Axios added

--- src/UM-ACCESS/LoginApp.js
+++ src/UM-ACCESS/LoginApp.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import '../Styles/Login.css';
+const axios = require('axios');
 
 
 class LoginApp extends Component
@@ -16,6 +17,21 @@ class LoginApp extends Component
         }
         this.usernameHandle = this.usernameHandle.bind(this);
         this.passwordHandle = this.passwordHandle.bind(this);
+        this.login = this.login.bind(this);
+    }
+    login()
+    {
+        axios({
+            method : 'post',
+            url : 'http://localhost:5000/',
+            data : {
+                username : this.state.username,
+                password : this.state.password,                
+            }
+        })
+        .then(function(data){
+            console.log(data.data);
+        });
     }
     usernameHandle(event)
     {
@@ -40,7 +56,7 @@ class LoginApp extends Component
                         <Col className='LoginBox'>
                             <input type='text' className='InputBox-1' placeholder='email'onChange={this.usernameHandle} /><br/>
                             <input type='password' className='InputBox-2'placeholder='password' /><br/>
-                            <input type='button' className='LoginButton' value='login'/> <br/><br/><br/>
+                            <input type='button' className='LoginButton' value='login' onClick={this.login}/> <br/><br/><br/>
                             <p>Don't have an account ? <a href='/register'>Register</a></p>
                         </Col>
                     </Row>
